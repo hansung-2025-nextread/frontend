285af8c8cd57889ade55086b0b7ec5cc35775072
sukhadukkha <parkjiho1229@naver.com>
2025-11-21 23:52:24 +0900

    refactor : SearchResponseData 변경(백엔드 API 응답 변화)

--- android/app/src/main/java/com/nextread/readpick/data/model/search/SearchResponseData.kt
+++ android/app/src/main/java/com/nextread/readpick/data/model/search/SearchResponseData.kt
@@ -4,22 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchResponseData(
-    val type: String, // 예: "SEARCH"
-    val matchedCategory: MatchedCategory?,
-    val books: List<SearchBookDto>,
-    val needsCategorySelection: Boolean,
-    val categoryOptions: List<CategoryOption>?
-)
-
-@Serializable
-data class MatchedCategory(
-    val id: Int,
-    val name: String
-)
-
-@Serializable
-data class CategoryOption(
-    val id: Int,
-    val name: String,
-    val similarity: Double
+    val books: List<SearchBookDto>
 )
